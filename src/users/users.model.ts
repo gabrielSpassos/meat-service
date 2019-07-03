8586d5922c7669d6aa3aadadc02b70d8c97cc930
gabriel passos <gabriel.passos@agibank.com.br>
2019-07-03 17:32:16 -0300

    :sparkles: add new route

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,10 +1,21 @@
 const users = [
-    {name: 'Gabriel Passos', email: 'gabriel@gmail.com'},
-    {name: 'Maria Souza', email: 'maria@gmail.com'}
+    {id: '1', name: 'Gabriel Passos', email: 'gabriel@gmail.com'},
+    {id: '2', name: 'Maria Souza', email: 'maria@gmail.com'}
 ];
 
 export class User {
     static findAll(): Promise<any[]> {
         return Promise.resolve(users);
+    }
+
+    static findById(id: string): Promise<any> {
+        return new Promise(resolve => {
+            const filtered = users.filter(users => users.id === id);
+            let user = undefined;
+            if(filtered.length > 0) {
+                user = filtered[0];
+            }
+            resolve(user);
+        });
     }
 }
